cbd625e914ee18eaad163db9138aaf2bb5682e36
Anees <aneesaboo123@gmail.com>
2025-10-28 13:57:06 +0530

    chnaged url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = (import.meta?.env?.VITE_API_BASE_URL || 'http://localhost:3000/api');
-
+// const API_BASE_URL = (import.meta?.env?.VITE_API_BASE_URL || 'http://localhost:3000/api');
+const API_BASE_URL = 'https://hackathon-everpulse.onrender.com/api';
 // Helper function to get auth token
 const getAuthToken = () => {
   const token = localStorage.getItem('token');
